e7d11523841f0fdf75c7452e5ab18d794ce97bb9
root <root@IZ23GDENIZ>
2024-01-20 20:52:04 +0000

    Crea usuarios y permisos

--- api-rest/xwiki_api.py
+++ api-rest/xwiki_api.py
@@ -1,134 +1,36 @@
 
-import requests
-from urllib.parse import urlencode
-import json
-import re
-
-print("\n\n## Comienza el test de conexion con las API REST de xwiki")
-
-# Define the CAS server URL and credentials
-CAS_SITE = "https://xwiki-cas:8443"
-CAS_URL_LOGIN = CAS_SITE + "/cas/login?service=https%3A%2F%2Fxwiki.mv%2Faplicaciones%2Fciberwiki%2Fbin%2Flogin%2FXWiki%2FXWikiLogin%3Fxredirect%3D%252Faplicaciones%252Fciberwiki%252Fbin%252Fview%252FMain%252F%26loginLink%3D1"
-XWIKI_SITE = "https://xwiki.mv/aplicaciones/ciberwiki"
-
-API_BASE = XWIKI_SITE + '/rest'
-API_PAGINA_PRIVADA = API_BASE + "/wikis/xwiki/spaces/privado/pages/WebHome"
-API_PAGINA_PUBLICA = API_BASE + "/wikis/xwiki/spaces/publico/pages/WebHome"
-API_CREAR_USUARIO = API_BASE + '/wikis/xwiki/spaces/XWiki/pages/{username}/objects'
-
-# Suprimir las advertencias de certificados SSL no verificados
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+from xwiki_api_class import XWikiAPI
 
 
-def auth_cas(username:str, password:str, debug:bool=False) -> requests.Session:
-    # Initialize session
-    session = requests.Session()
-    response = session.head(XWIKI_SITE, verify=False)
 
-    # Captura el cas_id    
-    response = session.get(CAS_URL_LOGIN, verify=False)
-    # Extract cas_id using regex
-    cas_id_match = re.search(r'name="execution" value="([^"]*)"', response.text)
-    if cas_id_match:
-        cas_id = cas_id_match.group(1)        
-    else:
-        print("Login ticket is empty.")
-        exit(1)
-    if debug:
-        print("\n** cas_id **\n")        
-        print(cas_id)
+def main():
+    cas_site = "https://xwiki-cas:8443"
+    xwiki_site = "https://xwiki.mv/aplicaciones/ciberwiki"
+    username = "admin"
+    password = "admin"
 
-    # Submit the CAS login form
-    data = {
-        "username": username,
-        "password": password,
-        "execution": cas_id,
-        "_eventId": "submit"
-    }        
-    response = session.post(CAS_URL_LOGIN, data=data, verify=False, allow_redirects=False)
+    xwiki_api = XWikiAPI(cas_site, xwiki_site, username, password)
 
-    # Check if login was successful
-    if 'Location' not in response.headers:
-        print("Cannot login. Check your credentials and URL.")
-        exit(1)
 
-    # Follow the redirection URL to get authenticated    
-    dest_ticket = response.headers['Location']
-    response = session.get(dest_ticket, verify=False)
-    if debug:
-        print(f'\n** Ticket **\n{dest_ticket}')    
-
-    return session
-
-def get_xwiki_form_token(session:requests.session) -> str:
-    ''' Devuelve el valor "XWiki-Form-Token" de la cabecera
-        de una respuesta a XWiki
-    '''
-    response = session.get(API_BASE, verify=False)
-    xwiki_form_token = response.headers.get('XWiki-Form-Token')
-    return xwiki_form_token
-
-def get_page(session:requests.session, page:str) -> str:
-    response = session.get(page, verify=False)
-    return response.text
-
-def crea_usuario(session:requests.session, xwiki_form_token:str, datos_usuario, username) ->requests.Response:
+    ## Crea una página
+    #xwiki_api.create_page('página3', content='Esto es la página 3')
     
-    url_create_user = API_CREAR_USUARIO.format(username=username)
-    headers = {'XWiki-Form-Token':xwiki_form_token}
-    response = session.post(url_create_user, data=datos_usuario, headers=headers, verify=False)
+    ## Muestra  el contenido de una pagina
+    # page_raw = xwiki_api.get_page('página3')
+    # if page_raw.status_code == 200:
+    #     page = json.loads(page_raw.text)
+    #     print(f"Pagina {page['title']}\nContenido: {page['content']}")        
     
-    # Check the response
-    if response.status_code == 201:
-        print('User created successfully.')
-    else:
-        print('Failed to create user:', response.status_code, response.text)
-
-    return response
-
-# Crear una página
-def crea_pagina(session:requests.session, xwiki_form_token:str, nombre_pagina:str, contenido:str) -> requests.Response:    
-    base_url = API_BASE + f'/wikis/xwiki/spaces/Main/pages/{nombre_pagina}'
-    headers = {'Content-Type': 'application/xml', 
-               'XWiki-Form-Token':xwiki_form_token}
+    ## Crea usuario
+    data_user = {'property#first_name': 'Pepe Luis',
+            'property#last_name': 'Rodriguez',
+            'property#email': 'user001@mail.com'                  
+            }
+    response = xwiki_api.create_user("user001", data_user=data_user)    
+    print(response.status_code)
     
-    data = f'''<page xmlns="http://www.xwiki.org">     
-    <title>{nombre_pagina}</title>
-    <syntax>xwiki/2.0</syntax>
-    <content>{contenido}</content>
-</page>'''
-    print(base_url)    
-    respuesta = session.put(base_url, data=data, headers=headers, verify=False)
-    return respuesta
 
-def main() -> None:    
-    api_pagina_privada = API_PAGINA_PRIVADA
-    session = auth_cas("admin", "admin")
-    xwiki_form_token = get_xwiki_form_token(session)
- 
 
-    # Muestra una página privada
-    # contenido_pagina_privada = get_page(session, api_pagina_privada)
-    # print(contenido_pagina_privada)
+if __name__ == "__main__":
+    main()
 
-    # Crea usuario
-    # Data for the new user
-    # username = 'ext-gdenaco'
-    # datos_usuario = {
-    #     'className': 'XWiki.XWikiUsers',
-    #     'property#first_name': 'Gonzalo',     # Replace with the user's first name
-    #     'property#last_name': 'Deniz',       # Replace with the user's last name
-    #     'property#email': 'gdeniz@inerza.com',  # Replace with the user's email
-    #     # Add other necessary properties here
-    # }    
-    # response = crea_usuario(session, xwiki_form_token, datos_usuario, username)
-  
-    # Ejemplo de uso
-    nombre_pagina = "Pagina 3"
-    contenido = "Este es el contenido de mi nueva página en XWiki."
-
-    # Crear una nueva página
-    response = crea_pagina(session, xwiki_form_token, nombre_pagina, contenido)
-    print(response.text)
-
-main()
---
+++ api-rest/xwiki_api_class.py
@@ -0,0 +1,217 @@
+import requests
+from urllib.parse import urlencode, quote
+import json
+import re
+
+# Suprime las advertencias de certificados SSL no verificados
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+class XWikiAPI:
+    def __init__(self, cas_site:str, xwiki_site:str, username:str, password:str) -> None:
+        self.cas_site = cas_site
+        self.xwiki_site = xwiki_site
+        self.api_base = xwiki_site + '/rest'
+        self.session = self.__auth_cas(username, password)
+        self.xwiki_form_token = self.__get_xwiki_form_token()
+
+    def __auth_cas(self, username:str, password:str) -> requests.Session:
+        ''' Autentifica contra el cas obteniendo un objeto de sesion'''
+        # Codifica la URL
+        xwiki_site_code = quote(self.xwiki_site)
+        xwiki_site_code2 = quote(xwiki_site_code)
+        service = f"{xwiki_site_code}%2Fbin%2Flogin%2FXWiki%2FXWikiLogin%3Fxredirect%3D%252F{xwiki_site_code2}%252Fbin%252Fview%252FMain%252F%26loginLink%3D1"        
+        cas_url_login = f"{self.cas_site}/cas/login?service={service}"
+        
+        # Inicializa sesion
+        session = requests.Session()
+        response = session.get(cas_url_login, verify=False)
+        cas_id_match = re.search(r'name="execution" value="([^"]*)"', response.text)
+
+        # Obtiene el cas_id
+        if not cas_id_match:
+            raise ValueError("Login ticket is empty.")
+        cas_id = cas_id_match.group(1)
+        data = {
+            "username": username,
+            "password": password,
+            "execution": cas_id,
+            "_eventId": "submit"
+        }
+
+        # Autentifica
+        response = session.post(cas_url_login, data=data, verify=False, allow_redirects=False)
+        if 'Location' not in response.headers:
+            raise ValueError("Cannot login. Check your credentials and URL.")
+        dest_ticket = response.headers['Location']
+        session.get(dest_ticket, verify=False)
+        return session
+
+    def __get_xwiki_form_token(self) -> str:
+        ''' Obtiene el xwiki_form_token necesario para trabajar con las api de xwiki'''
+        response = self.session.get(self.api_base, verify=False)
+        return response.headers.get('XWiki-Form-Token')
+        
+    def _handle_page_response(self, response:requests.Response) -> None:
+        ''' Gestiona los códigos de respuesta de una página'''
+        print(f"STATUS CODE = {response.status_code}")
+        if response.status_code == 200:
+            print('The request was successful.')
+        elif response.status_code == 201:
+            print('The page was created.')
+        elif response.status_code == 202:
+            print('The page was update.')            
+        elif response.status_code == 204:
+            print('The page was delete.')               
+        elif response.status_code == 304:
+            print('The page was not modified.')        
+        else:
+            raise ValueError(f"Error: \n{response.text}\nCodigo: {response.status_code}" )
+
+
+    def _handle_object_response(self, response:requests.Response) -> None:
+        ''' Gestiona los códigos de respuesta de un objecto'''
+        if response.status_code == 200:
+            print('The request was successful.')
+        elif response.status_code == 201:
+            print('The object was created.')
+        elif response.status_code == 202:
+            print('The object was update.')            
+        elif response.status_code == 204:
+            print('The object was delete.')                     
+        else:
+            raise ValueError(f"Error: \n{response.text}\nCodigo: {response.status_code}" )
+          
+
+    def get_page(self, page:str, wiki:str='xwiki', space:str='Main') -> requests.Response:
+        ''' Obtiene un request.Rsponse de una pagina
+                page - Id de la pagina
+                wiki - Seleccion de wiki en el caso que esté configurado las multiwiki
+                space - Nombre del espacio donde se encuentra la pagina                
+            API que utiliza https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/Main/pages/pagina1
+        '''        
+        url = f"{self.api_base}/wikis/{wiki}/spaces/{space}/pages/{page}?media=json"                         
+        response = self.session.get(url, verify=False)  
+        print(f"\nGET = {url}")      
+        self._handle_page_response(response)        
+        return response
+    
+
+    def get_objects(self, page:str, class_name:str='', wiki:str='xwiki', space:str='Main') -> str:
+        ''' Obtiene una lista de objetos de una pagina
+                page - Id de la pagina
+                class_name - Muestra los objetos de una clase
+                wiki - Seleccion de wiki en el caso que esté configurado las multiwiki
+                space - Nombre del espacio donde se encuentra la pagina                
+            API que utiliza https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/Main/pages/pagina1
+        '''         
+        url = f"{self.api_base}/wikis/{wiki}/spaces/{space}/pages/{page}/objects{class_name}?media=json"                 
+        response = self.session.get(url, verify=False)
+        print(f"\nGET = {url}")
+        self._handle_page_response(response)
+        return response      
+    
+    def create_page(self, page:str, content:str='', data:str=None, wiki:str='xwiki', space:str='Main') -> requests.Response:        
+        ''' Crea una página.
+                page_name - Nombre de la pagina
+                content   - Contenido de la pagina
+                data      - Estructura en json del contenido de una pagina. 
+                            Si no se especifica se utiliza unos datos básicos por defecto
+                wiki - Seleccion de wiki en el caso que esté configurado las multiwiki
+                space - Nombre del espacio donde se encuentra la pagina
+                            
+            Informacion sobre la estructura de la pagina xml:
+                https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/Main/pages/<nombre_pagina>        
+        '''        
+        url = f"{self.api_base}/wikis/{wiki}/spaces/{space}/pages/{page}" 
+        headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 
+                   'XWiki-Form-Token': self.xwiki_form_token}
+
+        # Datos por defecto de una pagina
+        data_default = {'id': {page},
+                        'title': {page},
+                        'syntax': 'xwiki/2.0',
+                        'content': {content}}
+        if data is None:
+            data = data_default
+        
+        response = self.session.put(url, data=data, headers=headers, verify=False)
+        print(f"\nPUT = {url}")        
+        self._handle_page_response(response)
+
+        return response
+
+
+    def create_object(self, page:str, data:dict, wiki:str='xwiki', space:str='XWiki') -> requests.Response:        
+        ''' Crea una página.
+                pages    - Nombre de la pagina
+                data      - Estructura en json del objeto. 
+                wiki - Seleccion de wiki en el caso que esté configurado las multiwiki
+                space - Nombre del espacio donde se encuentra la pagina
+                            
+            Informacion sobre la estructura de la pagina xml:
+                https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/XWiki/pages/<nombre_pagina>/objects        
+        '''        
+        url = f"{self.api_base}/wikis/{wiki}/spaces/{space}/pages/{page}/objects" 
+        print(f"\nPOST = {url}")
+
+        headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 
+                   'XWiki-Form-Token': self.xwiki_form_token}        
+        response = self.session.post(url, data=data, headers=headers, verify=False)
+        self._handle_page_response(response)
+        return response
+
+
+    def create_user(self, username:str, group:str='XWikiAllGroup', data_user:dict={}, data_rights:dict={}, wiki:str='xwiki', space:str='XWiki') -> requests.Response:    
+        ''' Crea un usuario.
+                username - Nombre del usuario
+                group - Nombre del grupo al que pertenece el usuario
+                data_user - Diccionario con los datos del usuario
+                data_rights - Diccionario con los datos de permisos
+                wiki - Seleccion de wiki en el caso que esté configurado las multiwiki
+                space - Nombre del espacio donde se encuentra la pagina                
+
+            Informacion sobre la estructura del objeto usuario en xml:
+                https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/Main/pages/admin/objects 
+                https://xwiki.mv/aplicaciones/ciberwiki/rest/wikis/xwiki/spaces/XWiki/pages/admin/?objects=true
+            
+            Ejemplo de data de usuario
+                data_user = {'
+                    'property#first_name': 'Pepe Luis',
+                    'property#last_name': 'Rodriguez',
+                    'property#email': 'user001@mail.com'          
+                    } 
+                data_rights = {'
+                    'property#groups': 'XWiki.XWikiAllGroup'         
+                    }  
+        '''
+
+        data_user_default = {'className': 'XWikiUsers',
+                             'property#active': '1'         
+        }
+        data_rights_default = {'className': 'XWikiRights',
+                                'property#allow': '1',
+                                'property#groups': 'XWiki.' + group,
+                                'property#users': 'XWiki.' + username   
+        }
+        data_group = {  'className': 'XWikiGroups',
+                        'property#member': 'XWiki.' + username}
+
+        # Combina los datos por defectos con los datos pasado por parámetros
+        data_user = data_user_default|data_user 
+        data_rights = data_rights_default|data_rights
+
+        # Crea la pagina
+        response = self.create_page(username, wiki=wiki, space=space)
+
+        # Crea el usuario
+        response = self.create_object(username, data=data_user)
+
+        # Crea los derechos
+        response = self.create_object(username, data=data_rights)
+
+        # Asigna el grupo
+        response = self.create_object(page='XWikiAllGroup', data=data_group)
+        
+        return response
+   
+    
